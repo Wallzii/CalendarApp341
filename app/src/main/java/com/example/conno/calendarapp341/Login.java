3afb88685c5278b3899b8d50139a42306d34a53e
Daniel Walls <dmwallzii@gmail.com>
2018-11-18 20:03:37 -0800

    Added Login activity.

---
+++ app/src/main/java/com/example/conno/calendarapp341/Login.java
@@ -0,0 +1,36 @@
+package com.example.conno.calendarapp341;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    TextView signUp = (TextView) findViewById(R.id.textView_signUp);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//        TextView signUp = (TextView) findViewById(R.id.textView_signUp);
+
+        MakeLinks.makeLinks(signUp, new String[] {
+                "Sign up!"
+        }, new ClickableSpan[] {
+                normalLinkClickSpan
+        });
+    }
+
+    //    signUp.setLinkTextColor(Color.BLUE); // default link color for clickable span, we can also set it in xml by android:textColorLink=""
+    ClickableSpan normalLinkClickSpan = new ClickableSpan() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "Normal Link", Toast.LENGTH_SHORT).show();
+        }
+
+    };
+}
---
+++ app/src/main/java/com/example/conno/calendarapp341/MakeLinks.java
@@ -0,0 +1,27 @@
+package com.example.conno.calendarapp341;
+
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.widget.TextView;
+
+public class MakeLinks {
+
+    static public void makeLinks(TextView textView, String[] links, ClickableSpan[] clickableSpans) {
+        SpannableString spannableString = new SpannableString(textView.getText());
+        for (int i = 0; i < links.length; i++) {
+            ClickableSpan clickableSpan = clickableSpans[i];
+            String link = links[i];
+
+            int startIndexOfLink = textView.getText().toString().indexOf(link);
+            spannableString.setSpan(clickableSpan, startIndexOfLink,
+                    startIndexOfLink + link.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        textView.setHighlightColor(
+                Color.TRANSPARENT); // prevent TextView change background when highlight
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(spannableString, TextView.BufferType.SPANNABLE);
+    }
+}
